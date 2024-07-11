d342e75afaa1f5be6a93ff40eb4d7aafd8d43c72
sehajbajaj2004 <sehajbajaj2004@gmail.com>
2024-07-11 16:48:10 +0530

    sese

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'; 
 
 const App = () => {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+        </div>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../style';
+import { navLinks } from '../constants';
+import {logo, menu, close} from '../assets';
 
 const Navbar = () => {
+
+  const [active, setActive] = useState('')
+
   return (
-    <div>Navbar</div>
+    <nav
+    className={`${styles.paddingx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+        to="/"
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0,0);
+        }}
+        >
+          <img src={logo} alt="Logo" className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Sehaj <span className='sm:block hidden text-red-500'>| Portfolio</span></p>
+          <ul
+          className='list-none flex flex-row gap-10'>
+            {navLinks.map((link) => (
+              <li key={link.id}
+              className={`${active === link.title ? "text-white" : "text-secondary"}`}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+           ))}
+          </ul>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
