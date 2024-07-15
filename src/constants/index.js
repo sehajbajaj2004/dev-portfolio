3b98b7c991a9e6785822303d02af26076e14ef77
sehajbajaj2004 <sehajbajaj2004@gmail.com>
2024-07-16 01:26:24 +0530

    Projects COmp

--- src/constants/index.js
+++ src/constants/index.js
@@ -105,18 +105,14 @@ const technologies = [{
         name: "figma",
         icon: figma,
     },
-    {
-        name: "docker",
-        icon: docker,
-    },
 ];
 
 const experiences = [{
-        title: "React.js Developer",
-        company_name: "Starbucks",
+        title: "Social Worker",
+        company_name: "Datri",
         icon: starbucks,
         iconBg: "#383E56",
-        date: "March 2020 - April 2021",
+        date: "June 2023 - July 2023",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -125,37 +121,11 @@ const experiences = [{
         ],
     },
     {
-        title: "React Native Developer",
-        company_name: "Tesla",
+        title: "Marketing Intern",
+        company_name: "UPES-Dehradun",
         icon: tesla,
         iconBg: "#E6DEDD",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#383E56",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#E6DEDD",
-        date: "Jan 2023 - Present",
+        date: "January 2024 - April 2024",
         points: [
             "Developing and maintaining web applications using React.js and other related technologies.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -189,37 +159,37 @@ const testimonials = [{
 ];
 
 const projects = [{
-        name: "Car Rent",
+        name: "Radiant(Space Endless Runner)",
         description: "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
         tags: [{
-                name: "react",
-                color: "blue-text-gradient",
-            },
-            {
-                name: "mongodb",
+                name: "unity",
                 color: "green-text-gradient",
             },
             {
-                name: "tailwind",
-                color: "pink-text-gradient",
+                name: "Csharp",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "blender",
+                color: "orange-text-gradient",
             },
         ],
         image: carrent,
         source_code_link: "https://github.com/",
     },
     {
-        name: "Job IT",
+        name: "Weather Application",
         description: "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
         tags: [{
                 name: "react",
                 color: "blue-text-gradient",
             },
             {
-                name: "restapi",
+                name: "tailwind",
                 color: "green-text-gradient",
             },
             {
-                name: "scss",
+                name: "js",
                 color: "pink-text-gradient",
             },
         ],
