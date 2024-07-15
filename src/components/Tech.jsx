799c570ee017332224c6ec052937c4c3f8a0e913
sehajbajaj2004 <sehajbajaj2004@gmail.com>
2024-07-15 16:25:52 +0530

    Tech Comp

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BallCanvas } from './canvas'
+import SectionWrapper from '../hoc/SectionWrapper'
+import { technologies } from '../constants'
+import { div } from 'three/examples/jsm/nodes/Nodes.js'
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technologie) => (
+        <div className='w-28 h-28' key={technologie.name}>
+          <BallCanvas icon={technologie.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
