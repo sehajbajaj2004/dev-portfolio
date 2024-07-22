f1feb5e9221586b2b96fff6e5ad4b26df774997b
sehajbajaj2004 <sehajbajaj2004@gmail.com>
2024-07-23 01:47:32 +0530

    Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="Logo" className='w-9 h-9 object-contain' />
+          <img src={logo} alt="Logo" className='w-10 h-10 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>
             Sehaj's &nbsp; | Portfolio
           </p>
